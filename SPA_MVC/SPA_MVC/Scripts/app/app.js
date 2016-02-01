9e9b40dbd03803aa5c79a23102ff93f3fe447e58
jhmt <jhashimot00@gmail.com>
2016-02-01 22:31:25 +1300

    Add $routeProvider and templates

--- SPA_MVC/SPA_MVC/Scripts/app/app.js
+++ SPA_MVC/SPA_MVC/Scripts/app/app.js
@@ -1,2 +1,19 @@
 ﻿/// <reference path="../angular.js" />
-var module = angular.module("StudentApp", ["stucontrollers", "sturesources"]);
+var module = angular.module("StudentApp",
+    ["ngRoute", "stucontrollers", "sturesources"]);
+
+module.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/home', {
+            templateUrl: 'templates/home.html',
+            controller: 'GetStudentsList'
+        }).when('/addstu', {
+            templateUrl: 'templates/addstudent.html',
+            controller: 'AddStudent'
+        }).when('/allstu', {
+            templateUrl: 'templates/allstudents.html',
+            controller: 'AllStudents'
+        }).otherwise({
+            redirectTo: '/home'
+        });
+}]);
