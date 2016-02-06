47dc1b4d94e1170deb735764bcba8ced8b0079ed
jhmt <jhashimot00@gmail.com>
2016-02-07 10:44:59 +1300

    DELETE method

--- SPA_MVC/SPA_MVC/Scripts/app/stucontroller.js
+++ SPA_MVC/SPA_MVC/Scripts/app/stucontroller.js
@@ -6,3 +6,49 @@ stucontrollers.controller("GetStudentsList",
         $scope.students = data;
     })
 });
+stucontrollers.controller("AddStudent", function ($scope, $http, $location) {
+    $scope.student = {
+        "FirstName": "",
+        "LastName": "",
+        "Age": "",
+        "Gender": "Male"
+    };
+    $scope.genders = ['Male', 'Female', 'Other'];
+    $scope.pattern = /^\s*\d+\s*$/;
+    $scope.Add = function () {
+        $http({ method: "POST", data: $scope.student, url: "/api/students" })
+        .success(function (data, status, headers, config) {
+            $scope.students = data;
+            $scope.student = {
+                "FirstName": "",
+                "LastName": "",
+                "Age": "",
+                "Gender": ""
+            };
+        });
+        alert("Studnet added successfully");
+        $location.path('/allstu');
+    }
+});
+
+
+stucontrollers.controller("AllStudents", function ($scope, $http) {
+
+    $scope.getAll = function () {
+        $http({ method: "GET", url: "/api/students" }).
+success(function (data, status, headers, config) {
+    $scope.students = data;
+});
+    }
+    $scope.getAll();
+    $scope.Remove = function (Id) {
+
+        $http({ method: "DELETE", url: "/api/students/" + Id })
+            .success(function () {
+                alert("Deleted Successfully..!!");
+                $scope.getAll();
+            });
+
+    }
+
+});
