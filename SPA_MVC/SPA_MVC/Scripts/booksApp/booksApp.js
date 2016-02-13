72a7753919eb4805844959885d2f3af38a2ece00
jhmt <jhashimot00@gmail.com>
2016-02-13 22:04:49 +1300

    Add books modules

---
+++ SPA_MVC/SPA_MVC/Scripts/booksApp/booksApp.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../angular.js" />
+var module = angular.module("BooksApp",
+    ["ngRoute", "booksControllers"]);
+
+module.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/books', {
+            templateUrl: 'templates/books.html',
+            controller: 'ListController'
+        }).otherwise({
+            redirectTo: '/books'
+        });
+}]);
---
+++ SPA_MVC/SPA_MVC/Scripts/booksApp/booksControllers.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../angular.js" />
+var booksControllers = angular.module("booksControllers", []);
+
+booksControllers.controller("ListController", function ($scope, $http) {
+    $http.get("http://localhost:4123/api/books").success(function (data) {
+        $scope.books = data;
+    });
+});
